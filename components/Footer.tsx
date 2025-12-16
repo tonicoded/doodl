80289dcf95fd4688f90daffffd3d62d5d81f053a
Anthony Verruijt <anthonyverruijt@mac.home>
2025-12-16 20:48:44 +0100

    gfd

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ export function Footer() {
       <div className="footerLinks" aria-label="Footer">
         <Link href="/privacy/">Privacy</Link>
         <Link href="/terms/">Terms</Link>
-        <a href="mailto:anthonyvvza@gmail.com">Support</a>
+        <Link href="/support/">Support</Link>
         <span>© DOODL. {new Date().getFullYear()}</span>
       </div>
     </footer>
